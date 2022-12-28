b91c4f529ea81c0f88fbfe3084279aad7484a5f8
eimamovic2 <eimamovic2@etf.unsa.ba>
2022-12-28 09:54:10 +0100

    Simple weather app for current weather and daily forecast

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7e30f9627amsh0e327d9a84685ffp1ff45fjsnffb376a2598f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "99af43530c00ab78143e9a5a7df15029";
